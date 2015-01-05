9b3c1293d0d929485cb16940d4490105b1ed2960
SeanRobinson159 <SeanRobinson159@gmail.com>
2015-01-05 14:23:24 -0700

    Set up Eclipse

---
+++ Week1/src/week1_1.java
@@ -0,0 +1,11 @@
+
+public class week1_1 {
+
+	public static void main(String[] args) {
+		int [] setA = args[i].split
+		for(int i = 0; i < args.length; i++) {
+            System.out.println(args[i]);
+        }
+	}
+
+}
