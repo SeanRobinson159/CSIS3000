e1496434f2c43198369a5e50b07b4e5115abd4bf
SeanRobinson159 <SeanRobinson159@gmail.com>
2015-02-06 14:56:27 -0700

    Starting RSA

--- Week3/src/bignumber.java
+++ Week3/src/bignumber.java
@@ -2,12 +2,24 @@ public class bignumber {
 
 	public static void main(String[] args) {
 
-		long x = 123;
-		for (int i = 1; i < 456; i++) {
-			x *= 123;
-			x = x % 789;
+		test(123456789);
+		test(1234567890);
+		test(12345678901L);
+
+	}
+
+	public static void test(long pow) {
+		double start, stop;
+		long n = 123456789;
+		long mod = 987654321;
+		long x = n;
+		start = System.currentTimeMillis();
+
+		for (int i = 1; i < pow; i++) {
+			x = x * n % mod;
 		}
-		System.out.println("123^456 % 789 = "+x);
+		stop = System.currentTimeMillis();
+		System.out.println(n+"^"+pow+" mod("+mod+") = "+ x + "\nTime: " + (stop - start) / 1000);
 	}
 
 }
