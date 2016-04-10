fef2f7bff965e7725a2ac0c923fb0d4097000b22
Sean Robinson <SeanRobinson159@gmail.com>
2016-04-10 09:12:10 -0600

    moved logic out of the switch, and into functions.

--- Connected Components/Connected Components/ViewController.swift
+++ Connected Components/Connected Components/ViewController.swift
@@ -7,16 +7,16 @@ enum Action {
 class ViewController: UIViewController {
 	@IBOutlet weak var drawingBoard: UIView!
 	@IBOutlet weak var componentsLabel: UILabel!
-	
+
 	var currentAction: Action?
 	var graph = Graph()
 	var currentNode: NodeView?
-	
+
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		currentAction = .AddNode
 	}
-	
+
 	@IBAction func AddNodeTouched(sender: UIBarButtonItem) {
 		currentAction = .AddNode
 	}
@@ -38,41 +38,52 @@ class ViewController: UIViewController {
 		componentsLabel.text = "\(graph.connectedComponents.count)"
 		graph.connectedComponents = []
 	}
-	
+
 	override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
 		guard let currentAction = currentAction else { return }
 		for touch in touches {
 			let tapPoint = touch.locationInView(self.drawingBoard)
 			switch currentAction {
 			case .AddNode:
-				let node = Node(name: "\(graph.nodes.count)", nodePosition: tapPoint)
-				graph.nodes.append(node)
-				let nodeView = NodeView(frame: CGRect(origin: CGPointMake(tapPoint.x - 25, tapPoint.y - 25), size: CGSize(width: 50, height: 50)), node: node)
-				self.drawingBoard.addSubview(nodeView)
-				
+				self.addNode(tapPoint)
 			case .RemoveNode:
-				checkIfNodeIsHit(tapPoint)?.removeFromSuperview()
+				self.removeNode(tapPoint)
 			case .Link:
-				if currentNode == nil {
-					self.currentNode = checkIfNodeIsHit(tapPoint)
-					self.currentNode?.circle.lineWidth = 5
-					self.currentNode?.setNeedsDisplay()
-				} else {
-					if let nextNode = checkIfNodeIsHit(tapPoint) {
-						nextNode.node.ConnectsTo(currentNode!.node)
-						
-						let linkView = LinkView(frame: drawingBoard.bounds, nodeViewA: currentNode!, nodeViewB: nextNode)
-						drawingBoard.insertSubview(linkView, atIndex: 0)
-						
-						currentNode?.circle.lineWidth = 3
-						self.currentNode?.setNeedsDisplay()
-						currentNode = nil
-					}
-				}
+				self.linkNodes(tapPoint)
 			}
 		}
 	}
-	
+
+	func addNode(tapPoint: CGPoint) {
+		let node = Node(name: "\(graph.nodes.count)", nodePosition: tapPoint)
+		graph.nodes.append(node)
+		let nodeView = NodeView(frame: CGRect(origin: CGPointMake(tapPoint.x - 25, tapPoint.y - 25), size: CGSize(width: 50, height: 50)), node: node)
+		self.drawingBoard.addSubview(nodeView)
+	}
+
+	func removeNode(tapPoint: CGPoint) {
+		checkIfNodeIsHit(tapPoint)?.removeFromSuperview()
+	}
+
+	func linkNodes(tapPoint: CGPoint) {
+		if currentNode == nil {
+			self.currentNode = checkIfNodeIsHit(tapPoint)
+			self.currentNode?.circle.lineWidth = 5
+			self.currentNode?.setNeedsDisplay()
+		} else {
+			if let nextNode = checkIfNodeIsHit(tapPoint) {
+				nextNode.node.ConnectsTo(currentNode!.node)
+
+				let linkView = LinkView(frame: drawingBoard.bounds, nodeViewA: currentNode!, nodeViewB: nextNode)
+				drawingBoard.insertSubview(linkView, atIndex: 0)
+
+				currentNode?.circle.lineWidth = 3
+				self.currentNode?.setNeedsDisplay()
+				currentNode = nil
+			}
+		}
+	}
+
 	func checkIfNodeIsHit(tapPoint: CGPoint) -> NodeView? {
 		for view in drawingBoard.subviews {
 			if view is NodeView {
